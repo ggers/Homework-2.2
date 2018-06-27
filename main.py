2cb1f53016be52229fbc4480b56aeeb75f0d6682
ggers <38072705+ggers@users.noreply.github.com>
2018-06-28 00:46:45 +0300

    Create main.py
    пока без проверки на длину слов(

---
+++ main.py
@@ -0,0 +1,47 @@
+
+import chardet
+
+
+def open_file_txt(target_file):
+    encode = None
+    strings = None
+    with open(target_file, "rb") as txt_file:
+        raw_info = txt_file.read()
+        encode = chardet.detect(raw_info)
+        print(encode)
+        strings = raw_info.decode(encode["encoding"])
+    return strings
+
+
+def count_frequency(target_file):
+    data = open_file_txt(target_file)
+    l_text = data.split()
+    s_text = set(data.split())
+    print(sorted(zip([l_text.count(w) for w in s_text], s_text), reverse=True)[0])
+
+
+def main():
+    answer = ""
+    while True:
+        print("\nПрограмма для работы с txt готова к работе.\nВыберите нужный файл и нажмите соответствующую цифру:\n",
+            "Для 'newsafr.txt' нажмите 1\n",
+            "Для 'newscy.txt' нажмите 2\n",
+            "Для 'newsfr.txt' нажмите 3\n",
+            "Для 'newsit.txt' нажмите 4\n",
+            "Для выхода нажмите q\n")
+        answer = input()
+        if answer == "1":
+            count_frequency("newsafr.txt")
+        elif answer == "2":
+            count_frequency("newscy.txt")
+        elif answer == "3":
+            count_frequency("newsfr.txt")
+        elif answer == "4":
+            count_frequency("newsit.txt")
+        elif answer == "q":
+            print("Программа для работы с txt завершена.")
+            break
+        else:
+            print("команда неверна попробуйте ещё раз")
+
+main()
